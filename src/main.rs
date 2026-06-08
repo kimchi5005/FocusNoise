e1f2cb4980632538c2efaf2686e91a8bb461d386
dev_kimchi <kimchi.main@proton.me>
2026-06-08 20:35:36 +0900

    Add application icon

--- src/main.rs
+++ src/main.rs
@@ -10,13 +10,16 @@ use eframe::egui;
 use crate::app::FocusNoiseApp;
 
 const APP_NAME: &str = "Focus Noise";
+const APP_ICON: &[u8] = include_bytes!("../assets/focus-noise.png");
 
 fn main() -> eframe::Result<()> {
+    let icon = eframe::icon_data::from_png_bytes(APP_ICON).ok();
     let options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
             .with_inner_size([420.0, 520.0])
             .with_min_inner_size([360.0, 460.0])
-            .with_title(APP_NAME),
+            .with_title(APP_NAME)
+            .with_icon(icon.unwrap_or_default()),
         ..Default::default()
     };
 
